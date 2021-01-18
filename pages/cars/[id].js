d912c3f34e2721903ed83d6f63d11ce2174eb084
KeyIndustries <stewartkey@hotmail.co.uk>
2021-01-18 18:13:47 +0000

    Car ssg from json file

---
+++ pages/cars/[id].js
@@ -0,0 +1,39 @@
+import { useRouter } from 'next/router'
+
+export default function Car({car}) {
+
+    const router = useRouter()
+    const { id } = router.query
+
+    return (<>
+        <head>
+            <title>{car.color} {car.id}</title>
+        </head>
+        <h1>Hello {id}</h1>
+        <img src={car.image} alt={"Image of an " + car.color + " " + car.id}/>
+    </>)
+}
+
+
+export async function getStaticProps({ params }) {
+    const req = await fetch(`http://localhost:3000/${params.id}.json`);
+    const data = await req.json();
+
+    return {
+        props: {car: data},
+    }
+}
+
+export async function getStaticPaths() {
+    const req = await fetch('http://localhost:3000/cars.json');
+    const data = await req.json();
+
+    const paths = data.map(car => {
+        return {params: {id: car}}
+    })
+
+    return {
+        paths,
+        fallback: false
+    };
+}
