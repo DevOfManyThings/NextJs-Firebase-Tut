c6d0b6e3d80bbfb25f03861075a875067875c9f4
KeyIndustries <stewartkey@hotmail.co.uk>
2021-01-18 18:16:42 +0000

    Commenting out ssg code

--- pages/cars/[id].js
+++ pages/cars/[id].js
@@ -22,25 +22,26 @@ export async function getServerSideProps({params}){
         props: {car: data},
     }
 }
-export async function getStaticProps({ params }) {
-    const req = await fetch(`http://localhost:3000/${params.id}.json`);
-    const data = await req.json();
-
-    return {
-        props: {car: data},
-    }
-}
-
-export async function getStaticPaths() {
-    const req = await fetch('http://localhost:3000/cars.json');
-    const data = await req.json();
-
-    const paths = data.map(car => {
-        return {params: {id: car}}
-    })
-
-    return {
-        paths,
-        fallback: false
-    };
-}
+//
+// export async function getStaticProps({ params }) {
+//     const req = await fetch(`http://localhost:3000/${params.id}.json`);
+//     const data = await req.json();
+//
+//     return {
+//         props: {car: data},
+//     }
+// }
+//
+// export async function getStaticPaths() {
+//     const req = await fetch('http://localhost:3000/cars.json');
+//     const data = await req.json();
+//
+//     const paths = data.map(car => {
+//         return {params: {id: car}}
+//     })
+//
+//     return {
+//         paths,
+//         fallback: false
+//     };
+// }
